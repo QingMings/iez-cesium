6558cf3fb1857ee174ee8923d3323415fed15bff
=QingMings <=>
2018-05-14 17:59:09 +0800

    增加获取点在模型上的坐标

--- src/components/cesiumViewer.vue
+++ src/components/cesiumViewer.vue
@@ -12,6 +12,7 @@ import PanoramaView from './PanoramaView.vue'
 import CesiumToolBarExtend from './CesiumToolBarExtend'
 import Cesium from 'cesium/Cesium'
 import 'cesium/Widgets/widgets.css'
+import FileSaver from 'file-saver'
 /* eslint-disable no-unused-vars */
 export default {
   components: {
@@ -54,7 +55,7 @@ export default {
   methods: {
     loadGeojson: function () {
       var vm = this
-      var serviceUrl = '../static/fenqu.geojson'
+      var serviceUrl = 'http://192.168.2.114:8080/360hot/GetPointsInfoServlet?sceneitems_id=58'
       var datasource1 = new Cesium.GeoJsonDataSource()
       var features = datasource1.load(serviceUrl)
       features.then(function (datasource) {
@@ -75,7 +76,7 @@ export default {
     load3dTiles: function () {
       var vm = this
       var tileset = new Cesium.Cesium3DTileset({
-        url: './static/data/Scene/Cesium_3D.json'
+        url: 'http://192.168.2.114:8085/data/Scene/Cesium_3D.json'
       })
       tileset.readyPromise.then(function (tileset) {
         vm.viewer.scene.primitives.add(tileset)
@@ -85,32 +86,19 @@ export default {
     getheight: function (target) {
       var vm = this
       var scene = vm.viewer.scene
-      // var pickedObject = scene.pick(vm.entities[0].position)
-      // if (scene.pickPositionSupported && Cesium.defined(pickedObject)) {
-      //   var cartesian = vm.viewer.scene.pickPosition(vm.entities[0].position)
-      //   console.info(cartesian)
-      //   if (Cesium.defined(cartesian)) {
-      var cartographic = Cesium.Cartographic.fromCartesian(vm.entities[0].position)
-      console.info(cartographic)
-      var longitudeString = Cesium.Math.toDegrees(cartographic.longitude).toFixed(2)
-      var latitudeString = Cesium.Math.toDegrees(cartographic.latitude).toFixed(2)
-      var heightString = cartographic.height.toFixed(2)
-      //
-      // labelEntity.position = cartesian
-      // labelEntity.label.show = true
-      // labelEntity.label.text =
-      //   'Lon: ' + ('   ' + longitudeString).slice(-7) + '\u00B0' +
-      //   '\nLat: ' + ('   ' + latitudeString).slice(-7) + '\u00B0' +
-      //   '\nAlt: ' + ('   ' + heightString).slice(-7) + 'm'
-      console.info('Lon: ' + ('   ' + longitudeString).slice(-7) + '\u00B0' +
-        '\nLat: ' + ('   ' + latitudeString).slice(-7) + '\u00B0' +
-        '\nAlt: ' + ('   ' + heightString).slice(-7) + 'm')
-
-      // labelEntity.label.eyeOffset = new Cesium.Cartesian3(0.0, 0.0, -cartographic.height * (scene.mode === Cesium.SceneMode.SCENE2D ? 1.5 : 1.0))
-
-      // foundPosition = true
-      // }
-      // }
+      var array = []
+      for (var entity of vm.entities) {
+        var entityPosition = entity.position.getValue(vm.viewer.clock.currentTime) // cartesian3
+        var canvasCoordinates = scene.cartesianToCanvasCoordinates(entityPosition) // cartesian2
+        var cartesian = vm.viewer.scene.pickPosition(canvasCoordinates)
+        var cartographic = Cesium.Cartographic.fromCartesian(cartesian)
+        var longitudeString = Cesium.Math.toDegrees(cartographic.longitude).toFixed(2)
+        var latitudeString = Cesium.Math.toDegrees(cartographic.latitude).toFixed(2)
+        var heightString = cartographic.height.toFixed(2)
+        array.push({'name': entity.properties.NAME.getValue(vm.viewer.clock.currentTime), 'height': heightString})
+      }
+      var blob = new Blob([JSON.stringify(array)], {type: 'application/json'})
+      FileSaver.saveAs(blob, 'point.json')
     },
     showHight: function () {
       var vm = this
